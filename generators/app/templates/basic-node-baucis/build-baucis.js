f59d79894f5d48dae3cc808c1346193751686d6e
perchopick <ivanhuay@gmail.com>
2017-03-16 10:31:18 -0300

    change passing modelName to baucis

--- generators/app/templates/basic-node-baucis/build-baucis.js
+++ generators/app/templates/basic-node-baucis/build-baucis.js
@@ -7,9 +7,9 @@ const decorators = require('./lib/decorators');
 function buildBaucis() {
     Object.keys(models).forEach((key) => {
         if (Object.keys(decorators).indexOf(key) !== -1) {
-            decorators[key](baucis.rest(models[key]));
+            decorators[key](baucis.rest(key));
         } else {
-            baucis.rest(models[key]);
+            baucis.rest(key);
         }
     });
     return baucis();
