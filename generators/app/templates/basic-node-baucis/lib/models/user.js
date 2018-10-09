e82e78b2716186de07b841e41cbea1614448627e
percho <ivanhuay@gmail.com>
2018-10-09 14:01:57 -0300

    shema subfolder removed

--- generators/app/templates/basic-node-baucis/lib/models/user.js
+++ generators/app/templates/basic-node-baucis/lib/models/user.js
@@ -1,5 +1,35 @@
 'use strict';
 const mongoose = require('mongoose');
-const UserSchema = require('./schemas/user-schema');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    full_name: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    }
+}, {
+    timestamps: true
+});
+
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
